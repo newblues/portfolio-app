67325f908b4baa12ceb5685b2e0f6cb63f25594d
Antoine Sauvage <new_blues@hotmail.com>
2019-04-24 17:35:03 +0200

    prettier eslint et modif home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import { FaAngleDoubleDown } from 'react-icons/fa';
 import '../App.css';
-import { HashLink as Link } from 'react-router-hash-link';
 
 import { connect } from 'react-redux';
-
 import { Button } from 'reactstrap';
-import { isAbsolute } from 'path';
+
+import bg from '../images/laptop.png';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      language: this.props.language
+      language: this.props.language,
     };
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.language !== this.props.language) {
       this.setState({ language: this.props.language });
-      console.log(
-        'TCL: Home -> constructor -> state.language',
-        this.state.language
-      );
+      console.log('TCL: Home -> constructor -> state.language', this.state.language);
       console.log('TCL: Home -> this.props.language', this.props.language);
     }
   }
@@ -31,25 +26,19 @@ class Home extends React.Component {
     console.log('je suis dans mon click!!!');
     this.props.setLanguage(e);
     this.setState({
-      language: this.props.language
+      language: this.props.language,
     });
   };
 
   render() {
     return (
       <div>
-        <div id='home' style={style.container}>
+        <div id="home" className="home">
           <div style={style.titleContainer}>
-            <h1
-              className='animated fadeInLeft delay-0.8s'
-              style={style.mainTitle}
-            >
+            <h1 className="animated fadeInLeft delay-0.8s" style={style.mainTitle}>
               ANTOINE SAUVAGE
             </h1>
-            <h2
-              className='animated fadeInRight delay-0.8s'
-              style={style.subTitle}
-            >
+            <h2 className="animated fadeInRight delay-0.8s" style={style.subTitle}>
               FULL-STACK DEVELOPPER
             </h2>
           </div>
@@ -57,21 +46,21 @@ class Home extends React.Component {
         <div style={style.buttonContainer}>
           {this.state.language == 'en' ? (
             <Button
-              value='fr'
+              value="fr"
               onClick={this.handleClick}
               outline
-              color='secondary'
-              className='shadow p-2 rounded btn-block'
+              color="secondary"
+              className="shadow p-2 rounded btn-block bg-light"
             >
               Français
             </Button>
           ) : (
             <Button
-              value='en'
+              value="en"
               onClick={this.handleClick}
               outline
-              color='secondary'
-              className='shadow p-2 rounded btn-block'
+              color="secondary"
+              className="shadow p-2 rounded btn-block "
             >
               English
             </Button>
@@ -83,23 +72,24 @@ class Home extends React.Component {
 }
 
 const style = {
-  container: {
-    position: 'relative',
-    // height: 'calc(100vh - 50px)',
-    height: '95vh',
-    background:
-      'linear-gradient(-225deg, rgba(0,101,168,0.6) 0%, rgba(0,36,61,0.6) 50%), url("https://picsum.photos/1000?image=20")',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundAttachment: 'fixed',
-    clipPath: 'polygon(0 0, 100% 0, 100% 80vh, 0 100%)',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'white',
-    fontFamily: 'Montserrat'
-  },
+  // container: {
+  //   position: 'relative',
+  //   // height: 'calc(100vh - 50px)',
+  //   height: '95vh',
+  //   background:
+  //     'linear-gradient(-225deg, rgba(0,101,168,0.6) 0%, rgba(0,36,61,0.6) 50%), `url(${bg})`',
+  //   backgroundImage: `url(${bg})`,
+  //   backgroundSize: 'cover',
+  //   backgroundPosition: 'center',
+  //   backgroundAttachment: 'fixed',
+  //   clipPath: 'polygon(0 0, 100% 0, 100% 80vh, 0 100%)',
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   color: 'white',
+  //   fontFamily: 'Montserrat',
+  // },
   buttonContainer: {
     width: 100,
     position: 'absolute',
@@ -107,25 +97,25 @@ const style = {
     justifyContent: 'center',
     alignItems: 'center',
     right: 40,
-    bottom: 40
+    bottom: 40,
   },
   mainTitle: {
     fontSize: 36,
     fontWeight: 600,
     letterSpacing: 5,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   subTitle: {
     fontSize: 17,
     fontWeight: 400,
     letterSpacing: 8,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   arrow: {
     position: 'absolute',
     right: 50,
     bottom: 40,
-    fontSize: '25px'
+    fontSize: '25px',
   },
   titleContainer: {
     display: 'flex',
@@ -137,8 +127,8 @@ const style = {
     paddingBottom: '50px',
     paddingRight: '50px',
     paddingLeft: '50px',
-    borderBottom: '1px solid',
-    borderTop: '1px solid'
+    // borderBottom: '1px solid',
+    // borderTop: '1px solid',
   },
   vline: {
     position: 'absolute',
@@ -147,29 +137,29 @@ const style = {
     height: 130,
     background: 'white',
     left: '50%',
-    marginLeft: '-1px'
-  }
+    marginLeft: '-1px',
+  },
 };
 
 function mapStateToProps(state) {
   return {
-    language: state.language
+    language: state.language,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    setLanguage: function(language) {
+    setLanguage(language) {
       // console.log('dispatch ------------->', language.target.value)
       dispatch({
         type: 'switchLanguage',
-        language: language.target.value
+        language: language.target.value,
       });
-    }
+    },
   };
 }
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(Home);
